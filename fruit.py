f1cb1d4e6f8392d17f1b13f1e4a09a565a7f700a
Grant Cheng <26587113+CatFish47@users.noreply.github.com>
2025-05-14 09:05:54 -0700

    swap out fruitstand/calc app for a textml sample app

--- fruit.py
+++ fruit.py
@@ -1,35 +1,38 @@
+import ray
 from ray import serve
-from ray.serve.handle import DeploymentHandle
+from ray.serve.drivers import DAGDriver
+from ray.serve.deployment_graph import InputNode
+from ray.serve.http_adapters import json_request
 
 # These imports are used only for type hints:
-from typing import Dict
+from typing import Dict, List
 from starlette.requests import Request
+from ray.serve.handle import RayServeDeploymentHandle
 
 
 @serve.deployment(num_replicas=2)
 class FruitMarket:
+
     def __init__(
         self,
-        mango_stand: DeploymentHandle,
-        orange_stand: DeploymentHandle,
-        pear_stand: DeploymentHandle,
+        mango_stand: RayServeDeploymentHandle,
+        orange_stand: RayServeDeploymentHandle,
+        pear_stand: RayServeDeploymentHandle,
     ):
         self.directory = {
-            "MANGO": mango_stand.options(use_new_handle_api=True),
-            "ORANGE": orange_stand.options(use_new_handle_api=True),
-            "PEAR": pear_stand.options(use_new_handle_api=True),
+            "MANGO": mango_stand,
+            "ORANGE": orange_stand,
+            "PEAR": pear_stand,
         }
-
+    
     async def check_price(self, fruit: str, amount: float) -> float:
         if fruit not in self.directory:
             return -1
         else:
             fruit_stand = self.directory[fruit]
-            return await fruit_stand.check_price.remote(amount)
-
-    async def __call__(self, request: Request) -> float:
-        fruit, amount = await request.json()
-        return await self.check_price(fruit, amount)
+            ref: ray.ObjectRef = await fruit_stand.check_price.remote(amount)
+            result = await ref
+            return result
 
 
 @serve.deployment(user_config={"price": 3})
@@ -41,10 +44,10 @@ class MangoStand:
         # This default price is overwritten by the one specified in the
         # user_config through the reconfigure() method.
         self.price = self.DEFAULT_PRICE
-
+    
     def reconfigure(self, config: Dict):
         self.price = config.get("price", self.DEFAULT_PRICE)
-
+    
     def check_price(self, amount: float) -> float:
         return self.price * amount
 
@@ -58,10 +61,10 @@ class OrangeStand:
         # This default price is overwritten by the one specified in the
         # user_config through the reconfigure() method.
         self.price = self.DEFAULT_PRICE
-
+    
     def reconfigure(self, config: Dict):
         self.price = config.get("price", self.DEFAULT_PRICE)
-
+    
     def check_price(self, amount: float) -> float:
         return self.price * amount
 
@@ -75,16 +78,27 @@ class PearStand:
         # This default price is overwritten by the one specified in the
         # user_config through the reconfigure() method.
         self.price = self.DEFAULT_PRICE
-
+    
     def reconfigure(self, config: Dict):
         self.price = config.get("price", self.DEFAULT_PRICE)
-
+    
     def check_price(self, amount: float) -> float:
         return self.price * amount
 
 
-mango_stand = MangoStand.bind()
-orange_stand = OrangeStand.bind()
-pear_stand = PearStand.bind()
+async def json_resolver(request: Request) -> List:
+    return await request.json()
 
-deployment_graph = FruitMarket.bind(mango_stand, orange_stand, pear_stand)
+
+with InputNode() as query:
+    fruit, amount = query[0], query[1]
+
+    mango_stand = MangoStand.bind()
+    orange_stand = OrangeStand.bind()
+    pear_stand = PearStand.bind()
+
+    fruit_market = FruitMarket.bind(mango_stand, orange_stand, pear_stand)
+
+    net_price = fruit_market.check_price.bind(fruit, amount)
+
+deployment_graph = DAGDriver.bind(net_price, http_adapter=json_request)
